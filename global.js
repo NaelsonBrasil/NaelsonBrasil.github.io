abbcc5e7f198efcfac8d36a3b88a38bb45b876a3
NaelsonBrasil <naelson.g.saraiva@gmail.com>
2020-02-13 17:48:08 -0400

    up

---
+++ global.js
@@ -0,0 +1,66 @@
+var btnTop = document.getElementById('m-top');
+
+window.setInterval(function() {
+
+    setTimeout(function(){
+
+        btnTop.style.transform = "scale(0.8)";
+    
+        setTimeout(function(){
+            btnTop.style.transform = "scale(0.9)";
+        },500);
+    
+    },700);
+
+}, 1000);
+
+var btnOn = document.getElementById('m-on');
+
+window.setInterval(function() {
+
+    setTimeout(function(){
+
+        btnOn.style.transform = "scale(0.8)";
+    
+        setTimeout(function(){
+            btnOn.style.transform = "scale(0.9)";
+        },500);
+    
+    },700);
+
+}, 1000);
+
+
+var btnTotal = document.getElementById('m-total');
+
+window.setInterval(function() {
+
+    setTimeout(function(){
+
+        btnTotal.style.transform = "scale(0.8)";
+    
+        setTimeout(function(){
+            btnTotal.style.transform = "scale(0.9)";
+        },500);
+    
+    },700);
+
+}, 1000);
+
+
+var btnSuper = document.getElementById('m-super');
+
+window.setInterval(function() {
+
+    setTimeout(function(){
+
+        btnSuper.style.transform = "scale(0.8)";
+    
+        setTimeout(function(){
+            btnSuper.style.transform = "scale(0.9)";
+        },500);
+    
+    },700);
+
+}, 1000);
+
